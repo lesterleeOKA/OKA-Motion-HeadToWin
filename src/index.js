b446702434ee154f7b0f90db8aaf0bb5decf3232
Lester Lee <82643100+lesterleefat0418@users.noreply.github.com>
2024-10-16 10:20:19 +0800

    added motion on/off btn

--- src/index.js
+++ src/index.js
@@ -278,6 +278,24 @@ function handleButtonClick(e) {
         State.changeState('leave');
       }, 500);
       break;
+    case View.motionBtn:
+      if (State.isSoundOn) {
+        Sound.play('btnClick');
+      }
+      State.gamePauseData.state = State.state;
+      State.gamePauseData.stateType = State.stateType;
+
+      if (View.motionBtn.classList.contains('on')) {
+        View.motionBtn.classList.add('off');
+        View.motionBtn.classList.remove('on');
+        View.renderer.showSkeleton = false;
+      }
+      else if (View.motionBtn.classList.contains('off')) {
+        View.motionBtn.classList.remove('off');
+        View.motionBtn.classList.add('on');
+        View.renderer.showSkeleton = true;
+      }
+      break;
     case View.musicBtn:
       if (State.state !== 'showMusicOnOff') {
         if (State.isSoundOn) {
@@ -381,6 +399,9 @@ function handleButtonTouch(e) {
     case View.exitBtn:
       View.exitBtn.classList.add('touched');
       break;
+    case View.motionBtn:
+      View.motionBtn.classList.add('touched');
+      break;
     case View.musicBtn:
       View.musicBtn.classList.add('touched');
       break;
@@ -410,6 +431,9 @@ function handleButtonTouchLeave(e) {
     case View.exitBtn:
       View.exitBtn.classList.remove('touched');
       break;
+    case View.motionBtn:
+      View.motionBtn.classList.remove('touched');
+      break;
     case View.musicBtn:
       View.musicBtn.classList.remove('touched');
       break;
@@ -435,6 +459,7 @@ function setupEventListeners() {
   const buttons = [
     View.startBtn,
     View.exitBtn,
+    View.motionBtn,
     View.musicBtn,
     View.backHomeBtnOfFinished,
     View.playAgainBtn,
